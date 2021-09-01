673e9b4f48ac166572dfe922f7bfb2f2a0d6114a
StarTinhs <89465347+StarTinhs@users.noreply.github.com>
2021-09-01 11:12:55 +0700

    AD

--- post/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../incf/config.php';
-  $token = $_GET['token'];
-  $cookie = $_GET['cookie'];
-  $time = $_GET['thoigian'];
-  $time  = (int)$time;
-  $camxuc = $_GET['camxuc'];
-  mysql_query("UPDATE `khachhang` SET `token`= '".$camxuc."' WHERE `id` ='5'")
-?>
